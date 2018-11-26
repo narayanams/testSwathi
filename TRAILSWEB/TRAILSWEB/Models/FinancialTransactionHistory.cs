12a8e747c9ecd748188010cd21f0c74a0154cb2c
Swathi Narayanam <swathi.narayanam@cfxway.com>
2018-11-26 14:04:28 -0500

    Revert "test"
    This reverts commit 520fd746d8d91c73999355670d4627816e577f13.

--- TRAILSWEB/TRAILSWEB/Models/FinancialTransactionHistory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.Serialization;
-
-namespace TRAILSWEB.Models
-{
-    [DataContract]
-    public class FinancialTransactionHistory : TransactionHistory
-    {
-    }
-}
--- TRAILSWEB/TRIPWEB/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TRIPWEB.MvcApplication" Language="C#" %>
